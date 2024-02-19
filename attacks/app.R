2a5274bb01c3226cae14e59ddba5bc6fc8f64b57
Kenatv <athule@uw.edu>
2024-02-20 05:53:05 +0700

    updte

--- attacks/app.R
+++ attacks/app.R
@@ -11,6 +11,7 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(shinythemes)
+library(leaflet)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(theme = shinytheme("cerulean"),
@@ -19,11 +20,17 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
     # Application title
     "Bear Attacks in North America",
 
-    tabPanel("Home", tags$img(src = "bear.png", height =50, width = 50), 
+    tabPanel("Home", tags$img(src = 'bear.png', height =50, width = 50), 
              h2 ("Welcome to statistical Data exploring Bear attacks"),
-             p("Through thorough analysis of historical bear attack data, we aim to pinpoint the locations where attacks are most likely, informing the public of potential dangers. Identifying the bear species with the highest fatality rates allows for targeted warnings, mitigating the risk of future incidents. This initiative underscores a pressing need to balance human safety with wildlife conservation, particularly concerning bear encounters that can result in severe consequences for individuals and communities alike. The narratives of those affected by bear attacks underscore the urgency of our mission. By contributing to this cause, we seek not only to prevent future tragedies but also to honor the memories of those impacted. 
+             p("Through thorough analysis of historical bear attack data, we aim to pinpoint the locations where attacks are most likely, informing the public of potential dangers. Identifying the bear species with the highest
+             fatality rates allows for targeted warnings, mitigating the risk of future incidents. This initiative underscores a pressing need to balance human safety with wildlife conservation, particularly concerning bear encounters that can result in severe consequences for individuals and communities alike. The narratives of those affected by bear attacks underscore the urgency of our mission. By contributing to this cause, we seek not only to prevent future
+             tragedies but also to honor the memories of those impacted. 
  
-We envision this project as a means to humanize bear attack statistics, fostering empathy and understanding of the risks involved. We aim to educate on the necessity of caution in bear habitats and inspire action toward safer human-bear coexistence. Our project is a step towards a future where humans and bears coexist more safely, emphasizing empowerment and preparedness for those navigating bear territories.", style = "font-size: 18px")),
+We envision this project as a means to humanize bear attack statistics, fostering empathy and understanding of the risks involved. We aim to educate on the necessity of caution in bear habitats and inspire action toward safer human-bear coexistence. Our project is a step towards a future where humans and bears coexist more safely, emphasizing empowerment and preparedness for those navigating bear territories.", style = "font-size: 18px"),
+             h2("This investigation is conducted with key questions to help us obtain information and avoid fatal events:"),
+             p("- Which region has the most amount of bear attacks?
+               - Are there specific geographic location that they attack in?
+               - Who are the usual targets of these attacks?", style = "font-size: 18px")),
             
              
     # Panel 2
@@ -36,12 +43,10 @@ We envision this project as a means to humanize bear attack statistics, fosterin
           plotOutput("barChart")
           
         ),
-        h2("Summary of Attacks"), p("This chart intends to answer the question of which regions are most prom
-                                    inent to have bear attacks since the 1900s. After analyzing the number of bear attack
-                                    s in each region, it is clear that there is a pattern. Regions in the northern part 
+        h2("Summary of Attacks"), 
+        p("This chart intends to answer the question of which regions are most prominent to have bear attacks since the 1900s. After analyzing the number of bear attacks in each region, it is clear that there is a pattern. Regions in the northern part 
                                     of North America is more prominant to fatal bear attacks. Example of this is shown by
-                                    regions such as Alaska or Alberta where the most common occurances of attacks take place.")
-    ),
+                                    regions such as Alaska or Alberta where the most common occurances of attacks take place.", style = "font-size: 18px")),
     
     #Panel 3 about the Lat and Long of data
     tabPanel("North America Trends", 
@@ -49,8 +54,9 @@ We envision this project as a means to humanize bear attack statistics, fosterin
                 plotOutput("scatterPlot")
                  
                )
-             )
-    
+             ),
+    tabPanel("Victim Types"),
+    tabPanel("Findings and Takeaways")
   )
 )
 
@@ -65,7 +71,7 @@ server <- function(input, output) {
         
         geom_point(data = Coordinate_and_person_type, aes(x = Longitude, y = Latitude, color = "red")) +
         
-        coord_map() +labs(title = "Latitude and Longitude of attacks", x = "Longitude", y = "Latitude")
+        coord_fixed(ratio = 1.3 ) + xlim(-169,-50)+ ylim(5,83)+labs(title = "Latitude and Longitude of attacks", x = "Longitude", y = "Latitude") + theme_minimal()
     })
     
     output$barChart <- renderPlot({
