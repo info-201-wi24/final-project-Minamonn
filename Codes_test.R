fa8e931b8fdaa99ee0550f45923dd2947527fbef
mtnkr <159337844+mtnkr@users.noreply.github.com>
2024-02-19 16:20:05 -0800

    Update Codes_test.R

--- Codes_test.R
+++ Codes_test.R
@@ -6,12 +6,10 @@ library(tidyverse)
 library(ggplot2)
 library(maps)
 library(mapproj)
-library(shiny)
-library(ggplot2)
+library(scales)
+library(plotly)
 
 # longitudinal locations for world
-# note the only other option is to use map data for just entire world since our coordinates
-# include more then just the united states
 world_shape <- map_data("world")
 # Longitudinal locations for bear attacks and descriptions
 bear_coords_df <- read.csv("bear_attacks.csv")
@@ -24,7 +22,7 @@ true_df <- left_join(NA_fatal_df, bear_coords_df, by = "Name")
 # cleaned libraries includes only includes basic information
 # excludes data without longitudinal coordinates
 Coordinate_and_person_type <- true_df %>% 
-  select(Name, Year, Latitude, Longitude, Location.x, Type.of.bear, Hunter, Hikers) %>% 
+  select(Name, gender, Year, Latitude, Longitude, Location.x, Type.of.bear, Hunter, Hikers) %>% 
   filter(!is.na(Latitude)) 
 
 # rounds latitude and longitude 
@@ -34,15 +32,14 @@ Coordinate_and_person_type <- Coordinate_and_person_type %>% mutate(across(c('La
 Coordinate_and_person_type <- Coordinate_and_person_type %>% mutate(region = str_replace(Location.x, ".*?,\\s*", ""))
 
 # creates a data frame with frequency of bear attacks per region
-# note for future (data frame is not states but regions in NA)
-state_counts <- Coordinate_and_person_type %>% group_by(region) %>% summarize(Number_of_attacks = n())
+region_counts <- Coordinate_and_person_type %>% group_by(region) %>% summarise(Number_of_attacks = n())
 
 # scatter plot of latitude vs longitude
 ggplot(Coordinate_and_person_type) + geom_point(aes(x = Latitude, y = Longitude))
 
 # bar graph of frequency of bar attacks per region (title needs to be updated)
-ggplot(state_counts, aes(x = region, y = Number_of_attacks)) + 
-  geom_bar(stat = "identity") + labs(title = "Numer of fatal attacks in each states")
+ggplot(region_counts, aes(x = region, y = Number_of_attacks)) + 
+  geom_bar(stat = "identity") + labs(title = "Numer of fatal attacks in each Region")
 
 # creates a scatter plot of the latitude and longitude of fatal bear attacks on a map
 # note while this works like intended more data wrangling is required to make it look better
@@ -55,3 +52,22 @@ ggplot(data = world_shape) +
   
   coord_map()
 
+# number of deaths per region per year data frame
+region_female_male_count <- Coordinate_and_person_type %>%
+  group_by(region) %>%
+  filter(gender == "female") %>% 
+  reframe(Female_deaths = n(), Coordinate_and_person_type %>%
+              group_by(Year) %>%
+              filter(gender == "male") %>% summarise(Male_deaths = n())
+  )
+
+# bar graph of deaths per region per year (males vs female deaths)
+female_male_deaths_by_region <- ggplot(data = region_female_male_count) +
+  geom_col(mapping = aes(Year, Female_deaths, fill = "Female")) +
+  geom_col(mapping = aes(Year, Male_deaths, fill = "Male")) +
+  labs(title = "Female and Males Bear Deaths by Year", x = "Year", y = "Number of Deaths", fill = "Gender") +
+  scale_x_continuous(breaks = seq(1901, 2018, 39)) +
+  scale_y_continuous(labels = label_number(scale_cut = cut_short_scale())) +
+  facet_wrap(~region)
+
+ggplotly(female_male_deaths_by_region)
